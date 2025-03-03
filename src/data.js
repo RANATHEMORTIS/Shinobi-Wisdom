42c81275eb3460b2c1b94e8a9cc21e5848e9b53c
Mortis <ranaroy9228@gmail.com>
2025-03-03 19:22:43 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Itachi Uchiha",
+    job: "Software Engineer",
+    image: "https://rukminim2.flixcart.com/image/850/1000/kvlaaa80/poster/l/p/8/medium-itachi-uchiha-akatsuki-mangakyou-mangakyou-sharingan-original-imag8gb8bpdfkyzy.jpeg?q=90&crop=false",
+    text: "🌑People live their lives bound by what they accept as correct and true. That’s how they define reality. But what does it mean to be ‘correct’ or ‘true’? Merely vague concepts… Their reality may all be a mirage.🌑 You and I are flesh and blood. I’m always going to be there for you, even if it’s only as an obstacle for you to overcome. Even if you do hate me.",
+  },
+  {
+    id: 2,
+    name: "Naruto",
+    job: "Graphic Designer",
+    image: "https://mrwallpaper.com/images/high/naruto-unleashing-powerful-chakra-energy-9tx26o8z45v32m37.webp",
+    text: "🔥 I'm not gonna run away, I never go back on my word! That's my nindo, my ninja way! 🔥 Hard work is worthless for those that dont believe in themselves. 🔥Failing doesnt give you a reason to give up, as long as you believe." ,
+  },
+  {
+    id: 3,
+    name: "Sasuke Uchiha",
+    job: "Marketing Manager",
+    image: "https://mrwallpaper.com/images/high/cool-sasuke-pure-power-scy1kmib74dvfreb.webp",
+    text: " 🔥 I have long since closed my eyes… My only goal is in the darkness🔥 I understand now. Even if I must take the devil’s fruit, I must gain power. I am an avenger.🔥 I spared your life on a whim, nothing more. But this time, my whim is telling me to finish what I started." ,
+  },
+  {
+    id: 4,
+    name: "Kakashi Hatake",
+    job: "Content Strategist",
+    image: "https://mrwallpaper.com/images/high/good-anime-naruto-kakashi-c4fjl0fryjubpm0h.webp",
+    text: "🌀 In the ninja world, those who break the rules are scum, but those who abandon their friends are worse than scum.🌀 A person grows up when he has to. When he ceases to whine and starts doing things on his own.",
+  },
+  {
+    id: 5,
+    name: "Minato Namikaze",
+    job: "Data Analyst",
+    image: "https://mrwallpaper.com/images/high/minato-with-rasengan-naruto-ipad-hmohorqnmvlz6t69.webp",
+    text: "⚡A Hokage is not someone who is chosen to be Hokage, but someone who is chosen by the village! ⚡Even if I can’t see you, no matter how far away you may be, I will always be watching you. (To Naruto)⚡ Naruto, no matter what happens, always remember that I will love you. ⚡ When you’re protecting something truly special to you, even if you die, you’ll have no regrets. ⚡ I won't fail. No matter what, I will protect the village! ⚡To be a parent is to have faith that one day your child will surpass you.",
+  },
+];
+
+export default reviews;
